c77ed5778b38b53a2945debb965c6429df1abffd
aprianfirlanda <aprianfirlanda@gmail.com>
2024-10-09 08:16:40 +0000

    fix shell script build docker

--- user-service/build_and_push-image.sh
+++ user-service/build_and_push-image.sh
@@ -1,3 +1,3 @@
-docker build -t aprianfirlanda/user-service:v1
+docker build -t aprianfirlanda/user-service:v1 .
 echo $PASSWORD_DOCKER_HUB | docker login -u aprianfirlanda --password-stdin
 docker push aprianfirlanda/user-service:v1
